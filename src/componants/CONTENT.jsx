98c21ae9ff9eba4b8359ac671ddc5185bceb9591
medsh <toos4950@gmail.com>
2025-10-03 20:51:06 +0330

    gray button

--- src/componants/CONTENT.jsx
+++ src/componants/CONTENT.jsx
@@ -9,10 +9,13 @@ function Content() {
             </div>
             <div className="main-content">
                 <div className="genre">
-
+                    <p>Score : <a>7.8</a></p>
+                    <p>Country : <a>USA</a></p>
+                    <p>Language : <a>English</a></p>
+                    <p>Duration : <a>120 min</a></p>
                 </div>
                 <div className="plot">
-
+                    <p> This is the long plot of the movie thats gonna be placed here . this movie belongs to the hosting server</p>
                 </div>
                 <div className="picture">
                     <img src="" />
